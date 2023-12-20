31fe0b76fc870f880aad14c798a704b540afa783
LennartBinkowski <60650430+MarkAureli@users.noreply.github.com>
2023-12-20 18:02:58 +0100

    Put copyssh back in

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,5 @@
 # Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="omz reload"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 
