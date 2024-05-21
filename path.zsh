257809605823261803ef39e2e2a7a9e8643da244
Lennart Binkowski <lennart.binkowski@yahoo.com>
2024-05-22 00:28:02 +0200

    prevent adding the same path multiple times upon shell reload

--- path.zsh
+++ path.zsh
@@ -1,5 +1,11 @@
+add_to_path() {
+  if [[ -d "$1" ]] && [[ ":$PATH:" != *":$1:"* ]] && [[ ":$PATH:" != "$1:"* ]]; then
+    export PATH="$1:$PATH"
+  fi
+}
+
 # Load dotfiles binaries
-export PATH="$DOTFILES/bin:$PATH"
+add_to_path "$DOTFILES/bin"
 
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
