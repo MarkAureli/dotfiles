3cea4ecd0f3eac4a0717683fbc6848ad963347ef
Lennart Binkowski <lennart.binkowski@yahoo.com>
2024-05-22 00:08:05 +0200

    fixed symlink error in fresh.sh, new macos setup for magic mouse, customized ssh.sh to write safer config file

--- ssh.sh
+++ ssh.sh
@@ -4,17 +4,18 @@ echo "Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+mkdir ~/.ssh/keys
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/keys/key_github
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 eval "$(ssh-agent -s)"
 
 touch ~/.ssh/config
-echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+echo "Host github github.com\n\tAddKeysToAgent yes\n\tUseKeychain yes\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/keys/key_github\n\nHost *\n\tIdentitiesOnly yes" | tee ~/.ssh/config
 
-ssh-add -K ~/.ssh/id_ed25519
+ssh-add -K ~/.ssh/keys/key_github
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
-echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+echo "run 'pbcopy < ~/.ssh/keys/key_github.pub' and paste that into GitHub"
