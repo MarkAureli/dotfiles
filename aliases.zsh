c7b6221880b0cc789825874d2470eb3cfb396ed5
Lennart Binkowski <lennart.binkowski@yahoo.com>
2025-06-05 21:16:24 +0200

    added shortcuts to desktop, documents, and downloads to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,9 @@ alias reloadshell="omz reload"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 
 # Directories
+alias desktop="cd $HOME/Desktop"
+alias documents="cd $HOME/Documents"
 alias dotfiles="cd $DOTFILES"
+alias downloads="cd $HOME/Downloads"
 alias library="cd $HOME/Library"
 alias projects="cd $HOME/Code"
