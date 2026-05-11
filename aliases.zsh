6c6ccc676de32dac3cf8b6603ec3de1f24b3d72d
lennartbinkowski <lennart.binkowski@itp.uni-hannover.de>
2026-05-11 09:13:24 +0200

    settings alias

--- aliases.zsh
+++ aliases.zsh
@@ -2,6 +2,7 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="omz reload"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias settings="open -a Settings"
 
 # Directories
 alias desktop="cd $HOME/Desktop"
